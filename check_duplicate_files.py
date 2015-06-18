b719b8a562711398efdc411e1bdbf4391c207218
Peter Hense <peter.hense@gmail.com>
2015-06-18 04:06:19 +0200

    removed multiprocessing

--- check_duplicate_files.py
+++ check_duplicate_files.py
@@ -7,12 +7,7 @@ Output is utf-8: If you use a windows command shell, please set it correctly
 """
 
 #TODO(zyrkon): Keyboard interrupt in main (strg+c)
-#BROKEN: Missing ErrorList / ErrorSet Proxy based on BaseManager
-#BROKEN: it seems multiple hashes are missing when using the multiprocessing-version
-#         of _hash (~1k lines difference in a 12k line output file from single process
-#         version, not the slightes clue how/why)
-#BROKEN: passing SIGINT to other processes requires SyncProxy, thought, that won't
-#        happen, becauce defaultdict(set) requires BaseManager with DictProxy :_(
+#TODO(zyrkon): implement perceptive hashing
 
 __author__      = 'Peter Hense (peter.hense@gmail.com)'
 __copyright__   = 'Copyright (c) 2015, Peter Hense'
@@ -27,49 +22,26 @@ import sys
 from argparse import ArgumentParser
 from argparse import ArgumentTypeError
 from collections import defaultdict
-from multiprocessing import Pool
-from multiprocessing.managers import BaseManager
-from multiprocessing.managers import DictProxy
 from stat import *
 from time import time
 
 
-class MyManager(BaseManager):
-    pass
-
-MyManager.register('defaultdict', defaultdict, proxytype = DictProxy)
-
-
-def generate_hashes(d_set_filesize, nprocs, hash):
+def generate_hashes(d_set_filesize, hash):
     mark_for_delete = []
     d_set_hash = defaultdict(set)
     errorlist = set()
 
     print('Creating filehashes...\n')
-    if nprocs == 1:          ## single process (default)
-        for key in d_set_filesize:
-            for file_path in d_set_filesize[key]:
-                _hash(file_path, d_set_hash, hash, errorlist)
-    else:                   ## multi-process version
-        pool = Pool(processes = nprocs)
-        mgr = MyManager()
-        mgr.start()
-        d_set_hash_proxy = mgr.defaultdict(set)
-        for key in d_set_filesize:
-            for file_path in d_set_filesize[key]:
-                pool.apply_async(_hash, (file_path, d_set_hash_proxy, hash))
-        pool.close()
-        pool.join()
-
-        d_set_hash = dict(d_set_hash_proxy)
-        errorlist = set(errorlist_proxy)
+    for key in d_set_filesize:
+        for file_path in d_set_filesize[key]:
+            _hash(file_path, d_set_hash, hash, errorlist)
 
     # Cleanup
     d_set_filesize.clear()
 
     for key in d_set_hash:
         if d_set_hash[key].__len__() == 1:
-                mark_for_delete.append(key)
+            mark_for_delete.append(key)
 
     for i in mark_for_delete:
         del d_set_hash[i]
@@ -77,9 +49,7 @@ def generate_hashes(d_set_filesize, nprocs, hash):
     return d_set_hash, errorlist
 
 
-def _hash (file_path, d_set_hash, hash, errorlist=[]):
-    temp = set()
-
+def _hash (file_path, d_set_hash, hash, errorlist):
     print('hashing: %s' % file_path)
     try:
         digest = open(file_path, 'rb').read()
@@ -97,16 +67,9 @@ def _hash (file_path, d_set_hash, hash, errorlist=[]):
         else:
             digest = hashlib.md5(digest).hexdigest()
 
-        temp = d_set_hash[digest].copy()
-        temp.add(file_path)
-
-        d_set_hash[digest] = temp
-        temp.clear
+        d_set_hash[digest].add(file_path)
     except:
-        temp = errorlist.copy()
-        temp.add(file_path)
-        errorlist = temp
-
+        errorlist.add(file_path)
     return
 
 
@@ -120,7 +83,7 @@ def scan_directories(directories, d_set_filesize, pHash):
         for path, subdirList, fileList in os.walk(root_dir):
             for fname in fileList:
                 qualified_filename = os.path.join(path, fname)
-                try:
+                try:            # possible denied permission for os.stat
                     st = os.stat(qualified_filename)
                     if S_ISREG(st.st_mode):
                         d_set_filesize[st.st_size].add(qualified_filename)
@@ -185,7 +148,6 @@ def write_output(d_set_hash, outfile, start_time, errorlist):
     return
 
 
-
 def readable_dir(prospective_dir):
     if not os.path.isdir(prospective_dir):
         raise ArgumentTypeError("readable_dir:{0} is not a valid path".format(prospective_dir))
@@ -205,29 +167,25 @@ def main():
     parser = ArgumentParser(description = 'Dublicate Checker')
     parser.add_argument('-i', action = 'append', dest = 'dir',
                         type = readable_dir,
-                        help = 'Add directory to list for duplicate search')
-
-    parser.add_argument('-j', action = 'store', dest = 'nprocs',
-                        default = 1, type = int,
-                        help = 'Number of Processes')
+                        help = 'add directory to list for duplicate search')
 
     parser.add_argument('--hash', action = 'store', dest = 'hash',
                         default = 'md5',
-                        help = 'Select Hash-Type (md5 (default), sha1, sha224, sha256, sha384, sha512)')
+                        help = 'select hash-type (md5 (default), sha1, sha224, sha256, sha384, sha512)')
 
     parser.add_argument('--disable-pHash', action = 'store', dest = 'pHash',
                         default = False,
-                        help = 'Disables perceptive Hashing on Images')
+                        help = 'disables perceptive hashing on images')
 
     parser.add_argument('outfile', nargs='?',
-                        help = 'Output File for found duplicates')
+                        help = 'output file for found duplicates')
 
     parser.add_argument('--version', action='version',
                         version='%(prog)s {version}'.format(version=__version__))
 
     args = parser.parse_args()
     scan_directories(args.dir, d_set_filesize, args.pHash)
-    d_set_hash, errorlist = generate_hashes(d_set_filesize, args.nprocs, args.hash)
+    d_set_hash, errorlist = generate_hashes(d_set_filesize, args.hash)
     write_output(d_set_hash, args.outfile, start_time, errorlist)
 
 
