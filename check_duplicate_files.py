a13dd7f599048420a97f0b55729649eeeb70b1fb
Peter Hense <peter.hense@gmail.com>
2015-06-20 10:41:10 +0200

    - increased overall robustness (more checks for IOError on file reads/writes) - catch encoding errors when writing to output file - sanitized _hash function (realized how stupid it is to work on the result-dict inside)

--- check_duplicate_files.py
+++ check_duplicate_files.py
@@ -14,6 +14,7 @@ __credits__     = ''        # ['List', 'of', 'programmers']
 __status__      = 'Development'     # Prototype / Development / Production
 __version__     = '0.5b'
 
+
 import codecs
 import hashlib
 import os
@@ -25,16 +26,22 @@ from collections import defaultdict
 from stat import *
 from time import time
 
+FILEREADERROR = 255
 
-def generate_hashes(d_set_filesize, hash):
+def generate_hashes(d_set_filesize, hashtype):
     mark_for_delete = []
     d_set_hash = defaultdict(set)
     errorlist = set()
+    hash = 0
 
     print('Creating filehashes...\n')
     for key in d_set_filesize:
         for file_path in d_set_filesize[key]:
-            _hash(file_path, d_set_hash, hash, errorlist)
+            hash = _hash(file_path, hashtype)
+            if hash != FILEREADERROR:
+                d_set_hash[hash].add(file_path)
+            else:
+                errorlist.add(file_path)
 
     # Cleanup
     d_set_filesize.clear()
@@ -49,33 +56,33 @@ def generate_hashes(d_set_filesize, hash):
     return d_set_hash, errorlist
 
 
-def _hash (file_path, d_set_hash, hash, errorlist):
+def _hash (file_path, hashtype):
     print('hashing: %s' % file_path)
     try:
         digest = open(file_path, 'rb').read()
 
-        if hash.lower() == 'sha1':
-            digest = hashlib.sha1(digest).hexdigest()
-        elif hash.lower() == 'sha224':
-            digest = hashlib.sha224(digest).hexdigest()
-        elif hash.lower() == 'sha256':
-            digest = hashlib.sha256(digest).hexdigest()
-        elif hash.lower() == 'sha384':
-            digest = hashlib.sha384(digest).hexdigest()
-        elif hash.lower() == 'sha512':
-            digest = hashlib.sha512(digest).hexdigest()
+        if hashtype.lower() == 'sha1':
+            return hashlib.sha1(digest).hexdigest()
+        elif hashtype.lower() == 'sha224':
+            return hashlib.sha224(digest).hexdigest()
+        elif hashtype.lower() == 'sha256':
+            return hashlib.sha256(digest).hexdigest()
+        elif hashtype.lower() == 'sha384':
+            return hashlib.sha384(digest).hexdigest()
+        elif hashtype.lower() == 'sha512':
+            return hashlib.sha512(digest).hexdigest()
         else:
-            digest = hashlib.md5(digest).hexdigest()
-
-        d_set_hash[digest].add(file_path)
+            return hashlib.md5(digest).hexdigest()
     except:
-        errorlist.add(file_path)
+        return FILEREADERROR
     return
 
 
 def scan_directories(directories, d_set_filesize, pHash):
     extensions = ('.jpg', 'jpeg', '.png', '.bmp')
     mark_for_delete = []
+    errorlist = set()
+    count = 0
 
     print('Scanning directories...')
 
@@ -87,13 +94,11 @@ def scan_directories(directories, d_set_filesize, pHash):
                     st = os.stat(qualified_filename)
                     if S_ISREG(st.st_mode):
                         d_set_filesize[st.st_size].add(qualified_filename)
+                        count += 1
                 except:
-                    pass
+                    errorlist.add(qualified_filename)
 
     # Statistic
-    count = 0
-    for key in d_set_filesize:
-        count += d_set_filesize[key].__len__()
     print('\nFiles found: %s' % count)
 
     # Cleanup (delete all files with unique silesize)
@@ -110,41 +115,66 @@ def scan_directories(directories, d_set_filesize, pHash):
         count += d_set_filesize[key].__len__()
     print('Possible candidates: %s\n' % count)
 
-    return
+    return errorlist
 
 
-def write_output(d_set_hash, outfile, start_time, errorlist):
+def write_output(d_set_hash, outfile, start_time, errorlist1, errorlist2):
     end_time = round(time() - start_time, 2)
+    write_errorlist = set()
+
     try:
-        with codecs.open(outfile, 'a', encoding="utf-8-sig") as f:
+        with codecs.open(outfile, 'w', encoding="utf-8") as f:
             f.write('\nThe Following File-Duplicates where found:')
             f.write('\n==========================================\n')
             for key in d_set_hash:
                 f.write('Hash: %s\n' %key)
                 for file_path in d_set_hash[key]:
-                    f.write('%s \n' % file_path)
+                    try:
+                        f.write('%s \n' % file_path)
+                    except:
+                        write_errorlist.add(file_path)
                 f.write('-------------------\n')
-            if errorlist.__len__() > 0:
+
+            if errorlist1.__len__() > 0 or errorlist2.__len__() > 0:
                 f.write('\nThe Following Files could not be accessed:')
                 f.write('\n==========================================\n')
-                for error in errorlist:
-                    f.write('%s\n' % error)
+                for error in errorlist1:
+                    try:
+                        f.write('%s\n' % error)
+                    except:
+                        write_errorlist.add(error)
+                for error in errorlist2:
+                    try:
+                        f.write('%s\n' % error)
+                    except:
+                        write_errorlist.add(error)
+
+            f.flush()
             f.write('\nExecution Time: %s seconds' % end_time)
 
     except:                 #IOError, UnicodeEncodeError
-        print('Error: Could not open Output File.\n')
+        print('- Error - Could not open Output File.\n')
         print('The Following File-Duplicates where found:')
         print('==========================================\n')
         for key in d_set_hash:
             for file_paths in d_set_hash[key]:
                 print(file_paths)
             print('--------------------')
-        if errorlist.__len__() > 0:
+        if errorlist1.__len__() > 0 or errorlist2.__len__() > 0:
             print('\nThe Following Files could not be accessed:')
             print('==========================================\n')
-            for error in errorlist:
+            for error in errorlist1:
                 print('%s\n' % error)
-    print('\nExecution Time: %s seconds' % end_time)
+            for error in errorlist2:
+                print('%s\n' % error)
+
+    if write_errorlist.__len__() > 0:
+        print('- Error - These files could not be written to output file:\n')
+        for write_error in write_errorlist:
+            print('%s\n' % write_error)
+        print('(Please check your filesystem encoding)\n')
+
+    print('Execution Time: %s seconds' % end_time)
     return
 
 
@@ -168,14 +198,15 @@ def main():
 
     d_set_filesize = defaultdict(set)
     d_set_hash = defaultdict(set)
-    errorlist = set()
+    errorlist1 = set()
+    errorlist2 = set()
 
     parser = ArgumentParser(description = 'Dublicate Checker')
     parser.add_argument('-i', action = 'append', dest = 'dir',
                         type = readable_dir,
                         help = 'add directory to list for duplicate search')
 
-    parser.add_argument('--hash', action = 'store', dest = 'hash',
+    parser.add_argument('--hash', action = 'store', dest = 'hashtype',
                         default = 'md5',
                         help = 'select hash-type (md5 (default), sha1, sha224, sha256, sha384, sha512)')
 
@@ -190,9 +221,9 @@ def main():
                         version='%(prog)s {version}'.format(version=__version__))
 
     args = parser.parse_args()
-    scan_directories(args.dir, d_set_filesize, args.pHash)
-    d_set_hash, errorlist = generate_hashes(d_set_filesize, args.hash)
-    write_output(d_set_hash, args.outfile, start_time, errorlist)
+    errorlist1 = scan_directories(args.dir, d_set_filesize, args.pHash)
+    d_set_hash, errorlist2 = generate_hashes(d_set_filesize, args.hashtype)
+    write_output(d_set_hash, args.outfile, start_time, errorlist1, errorlist2)
 
 
 if __name__ == '__main__':
