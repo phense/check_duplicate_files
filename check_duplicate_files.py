d158b7e110f1f65fed2b04bd64eb76b8279c6dfb
Peter Hense <peter.hense@gmail.com>
2015-06-20 11:52:59 +0200

    included query to ask user to continue before hashing

--- check_duplicate_files.py
+++ check_duplicate_files.py
@@ -16,6 +16,7 @@ __version__     = '0.5b'
 
 
 import codecs
+import datetime
 import hashlib
 import os
 import signal
@@ -24,7 +25,7 @@ from argparse import ArgumentParser
 from argparse import ArgumentTypeError
 from collections import defaultdict
 from stat import *
-from time import time
+
 
 FILEREADERROR = 255
 
@@ -54,28 +55,6 @@ def generate_hashes(d_set_filesize, hashtype):
         del d_set_hash[i]
 
     return d_set_hash, errorlist
-
-
-def _hash (file_path, hashtype):
-    print('hashing: %s' % file_path)
-    try:
-        digest = open(file_path, 'rb').read()
-
-        if hashtype.lower() == 'sha1':
-            return hashlib.sha1(digest).hexdigest()
-        elif hashtype.lower() == 'sha224':
-            return hashlib.sha224(digest).hexdigest()
-        elif hashtype.lower() == 'sha256':
-            return hashlib.sha256(digest).hexdigest()
-        elif hashtype.lower() == 'sha384':
-            return hashlib.sha384(digest).hexdigest()
-        elif hashtype.lower() == 'sha512':
-            return hashlib.sha512(digest).hexdigest()
-        else:
-            return hashlib.md5(digest).hexdigest()
-    except:
-        return FILEREADERROR
-    return
 
 
 def scan_directories(directories, d_set_filesize, pHash):
@@ -118,8 +97,7 @@ def scan_directories(directories, d_set_filesize, pHash):
     return errorlist
 
 
-def write_output(d_set_hash, outfile, start_time, errorlist1, errorlist2):
-    end_time = round(time() - start_time, 2)
+def write_output(d_set_hash, outfile, exec_time, errorlist1, errorlist2):
     write_errorlist = set()
 
     try:
@@ -150,7 +128,7 @@ def write_output(d_set_hash, outfile, start_time, errorlist1, errorlist2):
                         write_errorlist.add(error)
 
             f.flush()
-            f.write('\nExecution Time: %s seconds' % end_time)
+            f.write('\nExecution Time: %s.%s seconds' % (exec_time.seconds, exec_time.microseconds))
 
     except:                 #IOError, UnicodeEncodeError
         print('- Error - Could not open Output File.\n')
@@ -164,9 +142,9 @@ def write_output(d_set_hash, outfile, start_time, errorlist1, errorlist2):
             print('\nThe Following Files could not be accessed:')
             print('==========================================\n')
             for error in errorlist1:
-                print('%s\n' % error)
+                print('%s' % error)
             for error in errorlist2:
-                print('%s\n' % error)
+                print('%s' % error)
 
     if write_errorlist.__len__() > 0:
         print('- Error - These files could not be written to output file:\n')
@@ -174,15 +152,61 @@ def write_output(d_set_hash, outfile, start_time, errorlist1, errorlist2):
             print('%s\n' % write_error)
         print('(Please check your filesystem encoding)\n')
 
-    print('Execution Time: %s seconds' % end_time)
+    print('Execution Time: %s.%s seconds' % (exec_time.seconds, exec_time.microseconds))
     return
 
 
-def signal_handler(signal, frame):
+def _hash (file_path, hashtype):
+    print('hashing: %s' % file_path)
+    try:
+        digest = open(file_path, 'rb').read()
+
+        if hashtype.lower() == 'sha1':
+            return hashlib.sha1(digest).hexdigest()
+        elif hashtype.lower() == 'sha224':
+            return hashlib.sha224(digest).hexdigest()
+        elif hashtype.lower() == 'sha256':
+            return hashlib.sha256(digest).hexdigest()
+        elif hashtype.lower() == 'sha384':
+            return hashlib.sha384(digest).hexdigest()
+        elif hashtype.lower() == 'sha512':
+            return hashlib.sha512(digest).hexdigest()
+        else:
+            return hashlib.md5(digest).hexdigest()
+    except:
+        return FILEREADERROR
+    return
+
+
+def _query_yes_no(question, default="yes"):
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
+
+
+def _signal_handler(signal, frame):
     sys.exit(1)
 
 
-def readable_dir(prospective_dir):
+def _readable_dir(prospective_dir):
     if not os.path.isdir(prospective_dir):
         raise ArgumentTypeError("readable_dir:{0} is not a valid path".format(prospective_dir))
     if os.access(prospective_dir, os.R_OK):
@@ -192,9 +216,9 @@ def readable_dir(prospective_dir):
 
 
 def main():
-    signal.signal(signal.SIGINT, signal_handler)
-    signal.signal(signal.SIGTERM, signal_handler)
-    start_time = time()
+    signal.signal(signal.SIGINT, _signal_handler)
+    signal.signal(signal.SIGTERM, _signal_handler)
+    start_time = datetime.datetime.now()
 
     d_set_filesize = defaultdict(set)
     d_set_hash = defaultdict(set)
@@ -203,7 +227,7 @@ def main():
 
     parser = ArgumentParser(description = 'Dublicate Checker')
     parser.add_argument('-i', action = 'append', dest = 'dir',
-                        type = readable_dir,
+                        type = _readable_dir,
                         help = 'add directory to list for duplicate search')
 
     parser.add_argument('--hash', action = 'store', dest = 'hashtype',
@@ -222,8 +246,17 @@ def main():
 
     args = parser.parse_args()
     errorlist1 = scan_directories(args.dir, d_set_filesize, args.pHash)
+
+    time_query = datetime.datetime.now()
+    if not _query_yes_no('Do you want to continue?', 'yes'):
+        sys.exit(0)
+    timedelta_query = datetime.datetime.now() - time_query     # timedelta
+
     d_set_hash, errorlist2 = generate_hashes(d_set_filesize, args.hashtype)
-    write_output(d_set_hash, args.outfile, start_time, errorlist1, errorlist2)
+
+    execution_time = datetime.datetime.now() - start_time         #timedelta
+    execution_time -= timedelta_query
+    write_output(d_set_hash, args.outfile, execution_time, errorlist1, errorlist2)
 
 
 if __name__ == '__main__':
