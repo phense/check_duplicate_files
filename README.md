46f23a6e3c8508e88f58353196b080ef9f2453e4
Peter Hense <peter.hense@gmail.com>
2015-07-07 19:46:13 +0200

    added comments, fixed some git errors, changed readme to markdown

--- README.md
+++ README.md
@@ -1,11 +1,31 @@
-Installation
-============
-
-use 'pip -r requirements.txt' to install package dependencies
+README
+-----------
 
 
-Usage
-=====
+# Description
+
+Finds all duplicate files in given directories using a hash-algorithm.
+
+After scanning the filesystem for possible duplicate files (all files with a unique
+filesize are dismissed, except for Images when selecting the perceptual hash
+algorithm). All candidate duplicate files are hashed. Through pre-filtering,
+this module is extremely fast on large file-sets since only a handful of files
+need to actually be hashed.
+
+The perceptual hashing algorithm is a difference hashing algorithm which computes
+the brightness between adjacent pixels, identifying the relative gradient direction.
+
+
+# Installation
+
+Use python3's pip to install the dependencies from requirements.txt:
+
+`python3 -m pip install tqdm`
+`python3 -m pip install Pillow`
+
+
+# Usage
+```
 usage: check_duplicate_files.py [-h] [-i DIR] [--hash HASHTYPE] [-p]
                                 [-o OUTFORMAT] [--version]
                                 outfile
@@ -26,12 +46,13 @@ optional arguments:
                         select output format (text, json, bash_rm, bash_link,
                         win_del, win_link)
   --version             show program's version number and exit
+```
 
+# NOTES
 
-NOTES
-=====
 You can for duplicates between multiple folders:
-check_cuplicate_files.py -i /path/to/folder1 -i /path/to/folder2 ./output.log
+
+`check_cuplicate_files.py -i /path/to/folder1 -i /path/to/folder2 ./output.log`
 
 This tool requires a console with UTF-8 support. If you are using the
 Windows Command Shell, please set the codepage accordingly via
--- check_duplicate_files.py
+++ check_duplicate_files.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-Output is utf-8: If you use a windows command shell, please list it correctly
-                 using 'chcp 65001'
+"""check_duplicate_files.py
+
+Finds all duplicate files in given directories using a hash-algorithm.
+
+After scanning the filesystem for possible duplicate files (all files with a unique
+filesize are dismissed, except for Images when selecting the perceptual hash
+algorithm). All possible candidate duplicate files are hashed. With pre-filtering,
+this module is extremely fast on large file-sets since only a handful of files
+need to actually hbe ashed.
+
+Standard use: python3 check_duplicate_files -i /some/folder ./out.txt
 """
 
 # FEATURE(zyrkon): ignore/include win/linux/mac hidden file
@@ -11,7 +19,7 @@ Output is utf-8: If you use a windows command shell, please list it correctly
 # FEATURE(zyrkon): find broken symbolic links
 # FEATURE(zyrkon): find empty files and directories
 # FEATURE(zyrkon): --size 20M-1G to find files between 20mb and 1gb (example)
-# TODO(zyrkon): commenting, lol
+# FEATURE(zyrkon): maybe a GUI
 
 __author__      = 'Peter Hense (peter.hense@gmail.com)'
 __copyright__   = 'Copyright (c) 2015, Peter Hense'
@@ -51,6 +59,18 @@ FILEREADERROR = 255
 
 
 def generate_hashes(filelist, image_list, hashtype, pHash):
+    """ Main-Module for handling all File-Hashing and saving the hash-results
+
+    Args:
+        filelist: List of file-paths to REGULAR FILES to run a normal hash-algorithm on
+        image_list: List of file-paths of images to run a perceptual hash-algorithm on
+        hashtype: hash-algorithm to use for normal files (default=md5)
+        pHash: boolean switch to activate perceptual image-hashing
+
+    Returns:
+        d_list_hash: dictionary with lists of files sorted by hash-value (key)
+        errorlist: list of files that could not be accessed / read
+    """
     d_list_hash = defaultdict(list)
     errorlist = []
 
@@ -62,7 +82,7 @@ def generate_hashes(filelist, image_list, hashtype, pHash):
             errorlist.append(file_path)
 
 
-    if pHash:            # perceptive image hashing
+    if pHash:            # perceptual image hashing
         d_list_hash_img = defaultdict(list)
 
         for file_path in tqdm(image_list, 'hashing images:', None, True):
@@ -73,7 +93,7 @@ def generate_hashes(filelist, image_list, hashtype, pHash):
                 errorlist.append(file_path)
 
         # calculate hamming-distance between all image-hashes to find
-        # outliners (hamming distance of two perceptive hashes < 4 means the images
+        # outliners (hamming distance of two perceptual hashes < 4 means the images
         # are basically the same)
         index_list = [key for key in d_list_hash_img]
         deleted_index_keys = []
@@ -88,7 +108,7 @@ def generate_hashes(filelist, image_list, hashtype, pHash):
                 if hash2 in deleted_index_keys:
                     continue
 
-                if _hamming_distance(hash1, hash2) < 3:
+                if _hamming_distance(hash1, hash2) < 4:
                     d_list_hash_img[hash1] += d_list_hash_img[hash2]
                     del d_list_hash_img[hash2]
                     deleted_index_keys.append(hash2)
@@ -104,6 +124,21 @@ def generate_hashes(filelist, image_list, hashtype, pHash):
 
 
 def _perceptive_hash(file_path, hash_size = 8):
+    """Calculates a hash-value from an image
+
+    Conversion uses a resized, grayscaled pixel-array of the image, converting
+    the pixel-array to a number-array (differences between neighboring pixels)
+    and finally converting these values to a hex-string of length hash_size
+
+    Args:
+        file_path:  Path to an Image File
+        hash_size:  Size of the generated hash string
+
+    Returns:
+        hash_string: generated hash string
+    """
+
+
     # if memory consumption is to high for many images, it is posisble to use
     # with open (file_path, 'rb') as f:
     #   image = Image.open(f)
@@ -140,6 +175,18 @@ def _perceptive_hash(file_path, hash_size = 8):
 
 
 def _hash(file_path, hashtype):
+    """Uses a specified standard hash-algorithm to hash a regular file
+
+    Args:
+        file_path: file_path to a regular file that can be hashed
+        hashtype: version of hash-algorithm, default = md5
+
+    Returns:
+        hash: hash-string of the hashed file
+
+    Raises:
+        Returns global const FILEREADERROR on IOError
+    """
     try:
         with open(file_path, 'rb') as f:
             contents = f.read()
@@ -150,12 +197,36 @@ def _hash(file_path, hashtype):
 
 
 def _hamming_distance(string1, string2):
+    """ Calculates the Hamming Distance of two strings, fast version
+
+    Args:
+        string1, string2: two strings of the same length
+
+    Returns:
+        Integer describing the Hamming Distance of the input strings
+    """
     assert len(string1) == len(string2)
     ne = operator.ne        # faster than '!=' and 'str.__ne__'
     return sum(map(ne, string1, string2))
 
 
 def scan_directories(directories, pHash):
+    """ creates file-lists from given directories
+
+    Recursively walks the given directories and their subdirectories, checking
+    all including files and their file-sizes. These are saved inside a dictionary
+    and pre-filtered by filesize. Optional separate handling of image-files.
+
+    Args:
+        directories: List of directories to crawl
+        pHash: boolean switch to active separate handling of image-files
+
+    Returns:
+        prefiltered_files: List of files with their file-paths
+        images: List of image-files if pHash is set, else an empty list
+        errorlist: List of files that could not be accessed
+    """
+
     extensions = ('.jpg', '.jpeg', '.png', '.bmp')
     d_list_filesize = defaultdict(list)
     images = []
@@ -214,6 +285,11 @@ def scan_directories(directories, pHash):
 
 
 def _delete_unique_entries(dictionary):
+    """ removes all Lists from a dictionary that contain a single element
+
+    Args:
+        dictionary a dictionary of type defaultdict(set) or defaultdict(list)
+    """
     mark_for_delete = []
 
     for key in dictionary:
@@ -226,7 +302,15 @@ def _delete_unique_entries(dictionary):
     return
 
 
-def write_output_text(d_list_hash, outfile, exec_time, errorlist):
+def write_output_text(d_list_hash, errorlist, outfile):
+    """ Writes result of this module in textform to a file
+
+    Args:
+        d_list_hash: found duplicates in form of a dictionary (key = hash-value)
+        outfile: the path and filename to write the output into (needs write-permission)
+        errorlist: list of files that could not be accessed
+    """
+
     write_errorlist = []
 
     try:
@@ -252,12 +336,8 @@ def write_output_text(d_list_hash, outfile, exec_time, errorlist):
                         write_errorlist.append(error)
 
             f.flush()
-            f.write('\nExecution Time: %s.%s seconds' % (exec_time.seconds, exec_time.microseconds))
-
     except:                 #IOError, UnicodeEncodeError
         print('\n- Error - Could not open Output File.\n')
-<<<<<<< HEAD
-=======
 
     if write_errorlist.__len__() > 0:
         print('- Error - These files could not be written to output file:\n')
@@ -265,53 +345,18 @@ def write_output_text(d_list_hash, outfile, exec_time, errorlist):
             print('%s\n' % os.path.normcase(write_error))
         print('(Please check your filesystem encoding)\n')
 
-    print('\nExecution Time: %s.%s seconds' % (exec_time.seconds, exec_time.microseconds))
     return
 
 
-def write_output_bash(d_list_hash, outfile, exec_time, create_link):
-    write_errorlist = []
+def write_output_bash(d_list_hash, outfile, create_link):
+    """ Writes result of this module as a bash script to a file
 
-    try:
-        with codecs.open(outfile, 'w', encoding='utf-8') as f:
-            f.write('#!/bin/bash\n\n')
-            f.write('# This script is machine generated and might do harm to your\n')
-            f.write('# running system.\n')
-            f.write('# Please check this script carefully before running\n')
-
-            if create_link:
-                f.write('printf "replacing duplicates with hardlinks..."\n')
-            else:
-                f.write('printf "deleting duplicates..."\n')
-            for key in d_list_hash:
-                try:
-                    original = os.path.normcase(d_list_hash[key][0])
-                    f.write('# ------------------\n')
-                    f.write('# Original: %s\n' % original)
-                    for copy in d_list_hash[key][1:]:
-                        f.write('rm %s\n' % copy)
-                        if create_link:
-                            f.write('ln %s %s\n' % (original, os.path.normcase(copy)))
-                except:
-                    write_errorlist.append(file_path)
-            f.flush()
-
-    except:                 #IOError, UnicodeEncodeError
-        print('\n- Error - Could not open Output File.\n')
->>>>>>> 7cc87f0a2b1e1981e7ac2536bdafab65950a7244
-
-    if write_errorlist.__len__() > 0:
-        print('- Error - These files could not be written to output file:\n')
-        for write_error in write_errorlist:
-            print('%s\n' % write_error)
-        print('(Please check your filesystem encoding)\n')
-
-    print('\nExecution Time: %s.%s seconds' % (exec_time.seconds, exec_time.microseconds))
-    return
-
-
-<<<<<<< HEAD
-def write_output_bash(d_list_hash, outfile, exec_time, create_link):
+    Args:
+        d_list_hash: found duplicates in form of a dictionary (key = hash-value)
+        outfile: the path and filename to write the output into (needs write-permission)
+        create_link: boolean switch to select, if a deleted file should be
+                     replaced by a hardlink
+    """
     write_errorlist = []
 
     try:
@@ -347,13 +392,18 @@ def write_output_bash(d_list_hash, outfile, exec_time, create_link):
             print('%s\n' % write_error)
         print('(Please check your filesystem encoding)\n')
 
-    print('\nExecution Time: %s.%s seconds' % (exec_time.seconds, exec_time.microseconds))
     return
 
 
-=======
->>>>>>> 7cc87f0a2b1e1981e7ac2536bdafab65950a7244
-def write_output_win(d_list_hash, outfile, exec_time, create_link):
+def write_output_win(d_list_hash, outfile, create_link):
+    """ Writes result of this module as a batch script to a file
+
+    Args:
+        d_list_hash: found duplicates in form of a dictionary (key = hash-value)
+        outfile: the path and filename to write the output into (needs write-permission)
+        create_link: boolean switch to select, if a deleted file should be
+                     replaced by a hardlink
+    """
     write_errorlist = []
 
     try:
@@ -389,22 +439,33 @@ def write_output_win(d_list_hash, outfile, exec_time, create_link):
             print('%s\n' % write_error)
         print('(Please check your filesystem encoding)\n')
 
-    print('\nExecution Time: %s.%s seconds' % (exec_time.seconds, exec_time.microseconds))
     return
 
 
-def write_output_json(d_list_hash, outfile, exec_time):
+def write_output_json(d_list_hash, outfile):
+    """ Writes result of this module as JSON to a file
+
+    Args:
+        d_list_hash: found duplicates in form of a dictionary (key = hash-value)
+        outfile: the path and filename to write the output into (needs write-permission)
+    """
     try:
         with codecs.open(outfile, 'w', encoding='utf-8') as f:
             json.dump(d_list_hash, f, ensure_ascii=False, indent=4)
     except:
         print('\n- Error - Could not write JSON Data to file')
 
-    print('\nExecution Time: %s.%s seconds' % (exec_time.seconds, exec_time.microseconds))
     return
 
 
 def _query_yes_no(question, default="yes"):
+    """User Console Interaction for Y/N Questions.
+
+    Args:
+        question: String containing a Question that needs User input
+        default: select the default answer of the question
+    """
+
     valid = {"yes": True, "y": True, "ye": True,
              "no": False, "n": False}
     if default is None:
@@ -433,12 +494,22 @@ def _signal_handler(signal, frame):
 
 
 def _readable_dir(prospective_dir):
+    """ Checks if a given string is a valid path on the file-system
+
+    Args:
+        prospective_dir: file-path as String
+    Returns:
+        prospective_dir if checks are passed
+    Raises:
+        ArgumentTypeError if checks fail
+    """
+
     if not os.path.isdir(prospective_dir):
-        raise ArgumentTypeError("readable_dir:{0} is not a valid path".format(prospective_dir))
+        raise ArgumentTypeError('readable_dir:{0} is not a valid path'.format(prospective_dir))
     if os.access(prospective_dir, os.R_OK):
         return prospective_dir
     else:
-        raise ArgumentTypeError("readable_dir:{0} is not a readable dir".format(prospective_dir))
+        raise ArgumentTypeError('readable_dir:{0} is not a readable dir'.format(prospective_dir))
 
 
 def main():
@@ -446,15 +517,7 @@ def main():
     signal.signal(signal.SIGTERM, _signal_handler)
     start_time = datetime.datetime.now()
 
-<<<<<<< HEAD
     parser = ArgumentParser(description = 'Check Duplicate Files')
-=======
-    d_list_filesize = defaultdict(list)
-    d_list_hash = defaultdict(list)
-    output = ['text', 'json', 'bash_rm', 'bash_link', 'win_del', 'win_link']
-
-    parser = ArgumentParser(description = 'Dublicate Checker')
->>>>>>> 7cc87f0a2b1e1981e7ac2536bdafab65950a7244
     parser.add_argument('-i', action = 'append', dest = 'dir',
                         type = _readable_dir,
                         help = 'add directory to list for duplicate search'
@@ -465,9 +528,9 @@ def main():
                         help = 'select hash-type (md5 (default), sha1, sha224, sha256, sha384, sha512)'
                         )
 
-    parser.add_argument('-p', '--perceptive-hashing', action = 'store_true',
+    parser.add_argument('-p', '--perceptual-hashing', action = 'store_true',
                         dest = 'pHash', default = False,
-                        help = 'enables perceptive hashing of images'
+                        help = 'enables perceptual hashing of images'
                         )
 
     parser.add_argument('-o', '--output-format', action = 'store', dest = 'outformat',
@@ -484,12 +547,12 @@ def main():
 
     args = parser.parse_args()
 
-    # disable perceptive hashing (normal hashes on all files) when PIL LIB could
+    # disable perceptual hashing (normal hashes on all files) when PIL LIB could
     # not be loaded and it is not enabled
     pHash = ((not IMG_LIB_ERROR) and args.pHash)
 
     if not pHash:
-        print('(Perceptive Image Scan disabled)')
+        print('(Perceptual Image Scan disabled)')
 
     # Scan all directories and find duplicates by filesize
     prefiltered_filelist, images, read_errors = scan_directories(args.dir, pHash)
@@ -511,29 +574,27 @@ def main():
     execution_time = datetime.datetime.now() - start_time       # timedelta
     execution_time -= timedelta_query                           # timedelta
 
-
     # write output
-<<<<<<< HEAD
     output = ['text', 'json', 'bash_rm', 'bash_link', 'win_del', 'win_link']
 
-=======
->>>>>>> 7cc87f0a2b1e1981e7ac2536bdafab65950a7244
     if args.outformat in output:
         if args.outformat == 'text':
-            write_output_text(d_list_hash, args.outfile, execution_time, read_errors)
+            write_output_text(d_list_hash, read_errors, args.outfile)
         elif args.outformat == 'json':
-            write_output_json(d_list_hash, args.outfile, execution_time)
+            write_output_json(d_list_hash, args.outfile)
         elif args.outformat == 'bash_rm':
-            write_output_bash(d_list_hash, args.outfile, execution_time, False)
+            write_output_bash(d_list_hash, args.outfile, False)
         elif args.outformat == 'bash_link':
-            write_output_bash(d_list_hash, args.outfile, execution_time, True)
+            write_output_bash(d_list_hash, args.outfile, True)
         elif args.outformat == 'win_del':
-            write_output_win(d_list_hash, args.outfile, execution_time, False)
+            write_output_win(d_list_hash, args.outfile, False)
         elif args.outformat == 'win_link':
-            write_output_win(d_list_hash, args.outfile, execution_time, True)
+            write_output_win(d_list_hash, args.outfile, True)
     else:
-        write_output_text(d_list_hash, args.outfile, execution_time, read_errors)
+        write_output_text(d_list_hash, read_errors, args.outfile)
 
+    print('\nExecution Time: %s.%s seconds' % (execution_time.seconds,
+                                            execution_time.microseconds))
 
     # done
     sys.exit(0)
